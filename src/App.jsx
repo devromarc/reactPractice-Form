c4cbd44cbc267df2a55e8a84b7797f72011696b9
devromarc <69194960+devromarc@users.noreply.github.com>
2025-04-10 20:26:51 +0800

    Validation upon submission using ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header.jsx";
-import StateLogin from "./components/StateLogin.jsx";
-// import Login from "./components/Login.jsx";
+// import StateLogin from "./components/StateLogin.jsx";
+import Login from "./components/Login.jsx";
 import Signup from "./components/Signup.jsx";
 
 function App() {
@@ -8,7 +8,8 @@ function App() {
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Login />
+        {/* <StateLogin /> */}
       </main>
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setemailIsInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
-  const enteredEmail = email.current.value;
-  const enteredPassword = password.current.value;
-
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(`Email: ${enteredEmail}, Password: ${enteredPassword}`);
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setemailIsInvalid(true);
+      return;
+    }
+
+    console.log("proceed.........");
   }
 
-  console.log(userCred);
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -21,6 +28,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input ref={email} id="email" type="email" name="email" />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
