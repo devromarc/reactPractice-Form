b0f94aa69d0da2928213f7f137aa28d719e9226e
devromarc <69194960+devromarc@users.noreply.github.com>
2025-04-10 20:09:50 +0800

    Adding validations on every keystroke and upon lost focus (blur)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 
 export default function Login() {
   const email = useRef();
--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
-export default function Login() {
+export default function StateLogin() {
   const [userCred, setuserCred] = useState({ email: "", password: "" });
+  const [didBlur, setdidBlur] = useState({
+    email: false,
+    password: false,
+  });
+
+  // this validation is too early
+  // TF condition to check whether it doesnt have @ symbol in the email and the user starts typing
+  const emailIsValid = userCred.email !== "" && !userCred.email.includes("@");
+  // using blur
+  const emailIsValidBLur = didBlur.email && !userCred.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,6 +26,11 @@ export default function Login() {
       ...prevState,
       [name]: value,
     }));
+    setdidBlur((prevState) => ({ ...prevState, [name]: false }));
+  }
+
+  function handleOnBlur(identifier) {
+    setdidBlur((prevState) => ({ ...prevState, [identifier]: true }));
   }
 
   console.log(userCred);
@@ -27,19 +42,22 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input
-            ref={email}
             id="email"
+            onBlur={() => handleOnBlur("email")}
             type="email"
             name="email"
             value={userCred.email}
             onChange={handleOnchange}
           />
+
+          <div className="control-error">
+            {emailIsValidBLur && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input
-            ref={password}
             id="password"
             type="password"
             name="password"
